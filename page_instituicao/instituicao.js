4b45fdecdc66db55999acfaa9306014d581e6a98
luiza helena <luhealmo.moz@gmail.com>
2025-04-16 21:44:35 -0300

    update

--- page_instituicao/instituicao.js
+++ page_instituicao/instituicao.js
@@ -1,6 +1,59 @@
-const menuBurguer = document.querySelector(".menu-nav");
-const nav = document.querySelector(".nav");
+function showSidebar(event) {
+    if (event) {
+        event.preventDefault(); // Impede que o link redirecione a página para o topo
+    }
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
 
-menuBurguer.addEventListener("click", () => nav.classList.toggle("active"));
+function hideSidebar(event) {
+    if (event) {
+        if(event.target.tagName.toLowerCase() === 'a') {
+            // Não impede o comportamento padrão se o alvo for um link
+        } else {
+            event.preventDefault(); // Impede que o link redirecione a página para o topo
+        }
+    }
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+}
 
-// fazer com que ao clicar nos links a aba se feche automaticamente 
+// Adiciona evento de clique a todos os links dentro do sidebar para fechar o menu
+document.querySelectorAll('.sidebar a').forEach(link => {
+    link.addEventListener('click', (event) => {
+        hideSidebar(event);
+    });
+});
+
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach( button =>{
+    button.addEventListener('click',()=>{
+        const faq = button.nextElementSibling;
+        const icon = button.children[1];
+
+        faq.classList.toggle('show');
+        icon.classList.toggle('rotate');
+    })
+} )
+
+document.addEventListener("DOMContentLoaded", function() {
+    const line = document.querySelector('.line');
+
+    // Função de callback para o Intersection Observer
+    const observerCallback = (entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                line.classList.add('visible'); // Adiciona a classe 'visible' quando a linha entra na viewport
+                observer.unobserve(entry.target); // Para observar o elemento após a primeira vez
+            }
+        });
+    };
+
+    // Cria uma instância do Intersection Observer
+    const observer = new IntersectionObserver(observerCallback);
+
+    // Começa a observar a linha
+    observer.observe(line);
+});
